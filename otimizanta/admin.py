e20aee5aa76a6218bd12231ca829eb1faa40107a
luis <luismingati@gmail.com>
2022-10-05 16:49:32 -0300

    Otimizanta site FIRST COMMIT

---
+++ otimizanta/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Beach, Restaurants
+
+admin.site.register(Beach)
+admin.site.register(Restaurants)
+# Register your models here.
---
+++ otimizanta/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Beach(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    image = models.ImageField()
+    description = models.TextField()
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class Restaurants(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    image = models.ImageField()
+    description = models.TextField()
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title
